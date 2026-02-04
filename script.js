a29b0f54a8815684b0884279a773edf7f8c626c8
ayush git <itstiwarianshu357@gmail.com>
2026-02-04 22:08:11 +0530

    digital clock

---
+++ script.js
@@ -0,0 +1,28 @@
+const hourEl=document.getElementById("hours");
+const minuteEl=document.getElementById("minutes");
+const secondEl=document.getElementById("seconds");
+const ampmEl=document.getElementById("ampm");
+
+function updateclock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+
+
+    if(h>12){
+        h=h-12;
+        ampm="PM";
+    }
+    hourEl.innerText=h<10?"0"+h:h;
+    minuteEl.innerText=m<10?"0"+m:m;
+    secondEl.innerText=s<10?"0"+s:s;
+    ampmEl.innerText=ampm;
+
+    setTimeout(() => {
+        updateclock();
+    }, 1000);
+
+}
+
+updateclock();
